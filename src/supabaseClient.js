fd5be407377a1d1ce10fb934884c77f5234303ad
User <user@example.com>
2026-02-19 16:08:55 +0800

    debug: add logs and verify supabase url

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// 請前往 https://supabase.com/ 註冊專案並取得以下資訊
-// 在 Dashboard -> Settings -> API 頁面下
+console.log("Initializing Supabase...");
+
 const supabaseUrl = 'https://wzgtrwaryzdlcnbrgchtf.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6Z3R3YXJ5emRsY25icmdjaHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NTQxMDYsImV4cCI6MjA4NzAzMDEwNn0.vyeMLkBoWtT_F2UulfmFVZrWlhi3-nIbY8YbFHF0zsE'
+
+console.log("Supabase URL is:", supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
